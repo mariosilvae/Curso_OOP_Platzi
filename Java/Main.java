f9bc428c87bd0e237ee483c0031c84f7cb535b9b
mariosilvae <mario.silvae@outlook.com>
2019-09-27 23:09:09 -0500

    Creando Objetos en Java.

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,17 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+
+        Car car = new Car();
+        car.license = "AMQ123";
+        car.driver = "Andres Herrera";
+        car.passenger = 4;
+        System.out.println("Car License: " + car.license);
+
+        Car car2 = new Car();
+        car2.license = "QWE567";
+        car2.driver = "Andrea Herrera";
+        car2.passenger = 3;
+        System.out.println("Car lincense: " + car2.license);
     }
 }
--- Java/Payment.java
+++ Java/Payment.java
@@ -1,3 +1,3 @@
 class Payment{
-    Intger id;
+    Integer id;
 }
