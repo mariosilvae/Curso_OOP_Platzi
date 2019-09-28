be0afbc2096bb26622f22157dacc9016ff8ec4e5
mariosilvae <mario.silvae@outlook.com>
2019-09-27 23:38:38 -0500

    Creando objetos en Python.

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,12 @@
+from car import Car
 if __name__ == "__main__":
     print("Hola Mundo")
+    car = Car()
+    car.license = "AMS234"
+    car.driver = "Andres Herrera"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "QWE567"
+    car2.driver = "Andrea Herrera"
+    print(vars(car2))
