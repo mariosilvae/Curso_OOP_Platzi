a6f8ce3e419348a3c9d564e4096b59a1c682a5be
mariosilvae <mario.silvae@outlook.com>
2019-09-27 23:59:57 -0500

    Declarando un Método Constructor en Java.

--- Java/Main.java
+++ Java/Main.java
@@ -2,16 +2,12 @@ class Main{
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
 
-        Car car = new Car();
-        car.license = "AMQ123";
-        car.driver = "Andres Herrera";
+        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123") );
         car.passenger = 4;
         car.printDataCar();
 
-        Car car2 = new Car();
-        car2.license = "QWE567";
-        car2.driver = "Andrea Herrera";
-        car2.passenger = 3;
+        Car car2 = new Car("AMQ456", new Account("Andrea Herrera", "AMD456") );
+        car2.passenger = 4;
         car2.printDataCar();
     }
 }
